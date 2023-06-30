0cdf86d2c1ac5ea83a5e86bed30f85fbc5f2ee79
Phan <duongboygh10@gmail.com>
2023-06-30 23:24:36 +0700

    add post entity and dto

---
+++ src/main/java/com/example/backendservice/domain/entity/Post.java
@@ -0,0 +1,29 @@
+package com.example.backendservice.domain.entity;
+
+import com.example.backendservice.domain.entity.common.DateAuditing;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "posts")
+public class Post extends DateAuditing {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(insertable = false, updatable = false, nullable = false, columnDefinition = "CHAR(36)")
+    private String id;
+
+    @Column(nullable = false)
+    private String caption;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_POST_USER"))
+    private User user;
+}
