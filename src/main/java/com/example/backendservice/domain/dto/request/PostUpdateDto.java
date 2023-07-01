48694e9744a4993af5cdea293947d78b960e6885
Phan <duongboygh10@gmail.com>
2023-07-02 01:00:10 +0700

    fix conflict post entity and dto

---
+++ src/main/java/com/example/backendservice/domain/dto/request/PostUpdateDto.java
@@ -0,0 +1,13 @@
+package com.example.backendservice.domain.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PostUpdateDto {
+    private String caption;
+}
