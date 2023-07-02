b5090cc70351d6a4bb2d66d4a673e6fafabc91ce
phuonghight <hoangphuong270703@gmail.com>
2023-07-03 01:18:35 +0700

    feat: 🎉 create entity and dto

---
+++ src/main/java/com/example/backendservice/domain/dto/response/FollowResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.backendservice.domain.dto.response;
+
+import com.example.backendservice.domain.dto.common.UserDateAuditingDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FollowResponseDto extends UserDateAuditingDto {
+
+    private String id;
+
+    private String from;
+
+    private String to;
+
+}
